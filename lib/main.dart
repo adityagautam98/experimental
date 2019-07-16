1c469abd889bf437a231b5fafc3af0f540d21bd7
adityagautam98 <aadityagautam98@gmail.com>
2019-07-16 12:32:29 +0530

    Card

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,19 @@
-import 'package:feedaback/background.dart';
-import 'package:feedaback/loginUi.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+
+import 'background.dart';
+import 'loginUi.dart';
 const _emojis = ['😞','1','2','3','4','😃'];
 
 
 void main() {
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
       .then((_) {
-    runApp(new MyApp());
+    runApp(MaterialApp(
+      home: MyApp(),
+    ));
   });
 }
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyApp(),
-    );
-  }
-}
-
 
 class MyApp extends StatefulWidget {
   MyApp({Key key, this.title}) : super(key: key);
@@ -37,7 +24,7 @@ class MyApp extends StatefulWidget {
   _MyAppState createState() => _MyAppState();
 }
 
-class _MyApp extends State<MyApp> {
+class _MyAppState extends State<MyApp> {
   double _value = 0;
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,73 +34,65 @@ class _MyApp extends State<MyApp> {
         children: <Widget>[
           Background(),
           Login(),
+          Container(
+            decoration: BoxDecoration(
+                gradient: new LinearGradient(
+                    colors: [Colors.orangeAccent, Colors.white30],
+                    begin: Alignment.bottomLeft,
+                    end: Alignment.topRight
+                )
+            ),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    '${_emojis[_value.toInt()]}',
+                    style: Theme.of(context).textTheme.display1,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Container(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text(_emojis[0], softWrap: true),
+                          Expanded(
+                            child: Slider(
+
+                              value: _value,
+                              //label: _emojis[_value.toInt()],
+                              min: 0.0,
+                              max: 5.0,
+                              divisions: 5,
+
+
+                              onChangeStart: (double value) {
+                                print('Start value is ' + value.toString());
+                              },
+                              onChangeEnd: (double value) {
+                                print('Finish value is ' + value.toString());
+                              },
+                              onChanged: (double value) {
+                                setState(() {
+                                  _value = value;
+                                });
+                              },
+                              activeColor: Colors.white,
+                              inactiveColor: Colors.black45,
+                            ),
+                          ),
+                          Text(_emojis[5], softWrap: true)
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
         ],
       ),);}
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Container(
-        decoration: BoxDecoration(
-            gradient: new LinearGradient(
-                colors: [Colors.orangeAccent, Colors.white30],
-                begin: Alignment.bottomLeft,
-                end: Alignment.topRight
-            )
-        ),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                '${_emojis[_value.toInt()]}',
-                style: Theme.of(context).textTheme.display1,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                child: Container(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text(_emojis[0], softWrap: true),
-                      Expanded(
-                        child: Slider(
-
-                          value: _value,
-                          //label: _emojis[_value.toInt()],
-                          min: 0.0,
-                          max: 5.0,
-                          divisions: 5,
-
-
-                          onChangeStart: (double value) {
-                            print('Start value is ' + value.toString());
-                          },
-                          onChangeEnd: (double value) {
-                            print('Finish value is ' + value.toString());
-                          },
-                          onChanged: (double value) {
-                            setState(() {
-                              _value = value;
-                            });
-                          },
-                          activeColor: Colors.white,
-                          inactiveColor: Colors.black45,
-                        ),
-                      ),
-                      Text(_emojis[5], softWrap: true)
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
   }
-}
 
